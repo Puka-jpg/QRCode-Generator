17ae2785570d5159d7ce985f54a9777721376293
Puka-jpg <pukar369@gmail.com>
2024-12-20 23:36:55 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import QRGenerator from "./QR Generator/QRGenerator";
+
+function App() {
+  return (
+    <>
+      <QRGenerator />
+    </>
+  );
+}
+
+export default App;
